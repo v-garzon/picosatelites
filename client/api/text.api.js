5ce2ed9cc9812cbc6e5d33473e878d757bdd6ae9
Victor Garzon <garzonsanchezvictor@gmail.com>
2023-01-13 20:36:48 +0100

    try num 999

--- client/api/text.api.js
+++ client/api/text.api.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 export const pullPageContentRequest = async (page) =>
-    await axios.get(`https:vgarzoon.com/content/${page}`)
+    await axios.get(`https://vgarzoon.com/content/${page}`)
 
 export const pullAllContentRequest = async () => {
-    const result = await axios.get('https:vgarzoon.com/allcontent')   // change to 'http://localhost:4000/...' when developing
+    const result = await axios.get('https://vgarzoon.com/allcontent')   // change to 'http://localhost:4000/...' when developing
     return result
 }
 
 export const pushNewContentRequest = async (content) => {
-    const result = await axios.post('https:vgarzoon.com/pushnewcontent', content)
+    const result = await axios.post('https://vgarzoon.com/pushnewcontent', content)
     return result
 }
